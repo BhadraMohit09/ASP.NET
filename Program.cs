15b61e86f67775a69067659c6a505da2963129ce
Bhadra Mohit <158451254+BhadraMohit09@users.noreply.github.com>
2024-06-23 13:28:36 +0530

    ASP .NET Lab-1@MAB

--- Program.cs
+++ Program.cs
@@ -9,26 +9,45 @@ public class Program
         //Candidate c1 = new Candidate();
         //c1.getData();
         //c1.Display();
-
+        
         //Staff s1 = new Staff();
         //s1.getData();
         //s1.Display();
-
+        
         //Account a1 = new Account();
         //a1.getData();
         //Console.WriteLine("==========");
         //a1.Display();
-
+        
         //Student s1 = new Student(101,"qwerty",5,9.9,9.9);
         //s1.Display();
-
+        
         //Rectangle r1 = new Rectangle(10,30);
         //r1.evaluate();
-
+        
         //Account_Details d1 = new Account_Details();
-       
+        
         //Interest i1 = new Interest();
         //i1.GetData();
         //i1.Display();
-        ;    }
+        
+        //Salary s1 = new Salary(100,25.32);
+        //s1.CalculateTotalSalary();
+        
+        //Distance d1 = new Distance(123.5,456.725);
+        //d1.AddDistances();
+        //d1.DisplayAddition();
+        
+        //Table t1 = new Table();
+        //t1.DisplayTableDetails();
+        
+        //Employee e1 = new Employee
+        //{
+        //    Name = "A"
+        //};
+        //SalaryB s1 = new SalaryB(1000,2000,3000);
+        //e1.basic_sal(5000);
+        //s1.Disp_sal();
+        //s1.Gross_sal();
+    }
 }
