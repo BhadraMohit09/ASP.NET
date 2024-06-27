699e8fbde2e3e0f768f799a9f6e74ca5b3ddd4fb
Bhadra Mohit <158451254+BhadraMohit09@users.noreply.github.com>
2024-06-27 17:34:49 +0530

    ASP. NET Lab-2@MAB

---
+++ OutOfBound.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2_MAB
+{
+    internal class OutOfBound
+    {
+        int[] array = new int[5];
+        public void ShowOutOfBound()
+        {
+            try
+            {
+                for (int i = 0; i < array.Length; i++)
+                {
+                    array[i] = Convert.ToInt32(Console.ReadLine());
+                }
+
+                Console.WriteLine(array[5]);
+            }
+            catch (IndexOutOfRangeException ex) {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+    }
+}
