6cc226c5f18eff71e6e2542d92c5caac50fbc950
Bhadra Mohit <158451254+BhadraMohit09@users.noreply.github.com>
2024-06-27 17:40:40 +0530

    ASP .NET Lab-2@MAB

---
+++ EvenException.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2_MAB
+{
+    internal class EvenException
+    {
+        public void ShowData()
+        {
+            try
+            {
+                int number = Convert.ToInt32(Console.ReadLine());
+
+                if (number % 2 != 0)
+                {
+                    throw new ArgumentException("Number is not even...");
+                }
+            }
+            catch (ArgumentException ex) { 
+                Console.WriteLine("An error occurred:: " + ex.Message);
+            }
+    }
+}
