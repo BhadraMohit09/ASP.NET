06302f2ee23ef3517722da0080a750775cf0b328
Bhadra Mohit <158451254+BhadraMohit09@users.noreply.github.com>
2024-06-27 17:39:53 +0530

    ASP .NET Lab-2@MAB

--- Program.cs
+++ Program.cs
@@ -7,21 +7,27 @@ public class Program
     {
         //DivisionError d1 = new DivisionError();
         //d1.showException();
-
+        
+        //OutOfBound o1 = new OutOfBound();
+        //o1.ShowOutOfBound();
+        
         //StringChange s1 = new StringChange();
         //s1.GetData();
-
+        
         //Calculate c1 = new Calculate();
         //int resultA = c1.SumOfTwo(10, 20);
         //int resultB = c1.SumOfThree(10,20,30);
         //Console.WriteLine("Result of Two numbers ==> " + resultA);
         //Console.WriteLine("Result of Three numbers ==> " + resultB);
-
+        
         //Result r1 = new Result();
         //r1.Addition();
         //r1.Subtraction();
-
-        StringMethods s1 = new StringMethods();
-        s1.showMethods();
+        
+        //StringMethods s1 = new StringMethods();
+        //s1.showMethods();
+        
+        //EvenException e1 = new EvenException();
+        //e1.ShowData();
     }
 }
